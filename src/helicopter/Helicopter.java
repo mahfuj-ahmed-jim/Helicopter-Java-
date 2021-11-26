f2ac14a8be4324b95327ae27bcffc21bbe005059
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2021-11-27 04:37:58 +0600

    Helicopter Structure

---
+++ src/helicopter/Helicopter.java
@@ -0,0 +1,44 @@
+package helicopter;
+
+public class Helicopter {
+
+    public static void main(String[] args) {
+        
+        // main wing
+        // for left wing 
+        System.out.print("ROFL:ROFL:");
+        // middle wing 
+        System.out.print("LOL");
+        // for right wing 
+        System.out.print(":ROFL:ROFL");
+        System.out.println(""); // next line
+        
+        // joint 
+        System.out.println("           A"); // next line
+        
+        // small wing upper
+        System.out.print("  L");
+        
+        // main body (upper)
+        System.out.println("    /--------"); // next line
+        
+        // small wing joint 
+        System.out.print(" LOL====");
+        
+        // window
+        System.out.println("      []\\"); // next line
+        
+        // small wing lower
+        System.out.print("  L");
+        
+        // lower part 
+        System.out.println("    \\         \\"); // next line
+        System.out.println("        \\_________|"); // next line
+        System.out.println("           I    I"); // next line
+        System.out.println("        ------------");
+        
+        System.out.println("\n\n\n\n\n\n\n\n\n\n");
+        
+    }
+    
+}
